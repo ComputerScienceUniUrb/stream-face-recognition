7465cff1f14cac0ed99d929cab9ccccafaec7a15
Bilashos <36623574+Bilashos@users.noreply.github.com>
2019-08-28 17:04:05 +0200

    modifiche
    centramento del viso con relativo ingrandimento, prove per l'inserimento di un immagine.png all'altezza degli occhi.

--- FaceDetection.py
+++ FaceDetection.py
@@ -13,6 +13,8 @@ cv2.createTrackbar("US", "Tracking",255,255,nothing)
 cv2.createTrackbar("UV", "Tracking",255,255,nothing)
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+img = cv2.imread('download2.png')
 cap = cv2.VideoCapture(0)
 
 while(True):
@@ -32,8 +34,8 @@ while(True):
     u_b = np.array([u_h, u_s, u_v])
 
     #mask = cv2.inRange(hsv, l_b, u_b)
-    print (frame)
-
+    #print (frame)
+    #print(frame.shape)
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
@@ -44,13 +46,38 @@ while(True):
         gr = cv2.cvtColor(frame[y:y+h, x:x+w], cv2.COLOR_BGR2BGRA)
         mask = np.zeros_like(frame)
 
+
         eyes = eye_cascade.detectMultiScale(roi_gray)
         for(ex,ey,ew,eh) in eyes:
             color = (255, 255, 255)
             cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), color, 2)
-            mask[y:y+h, x:x+w] = frame[y:y+h, x:x+w]
-        #frame[y:y+h, x:x+w] = gr
+            #b = np.delete(roi_color[ey:ey+eh, ex:ex+ew], 3, axis=1)
+            #print(b[0],b[1],)
+            a = cv2.resize(img, (frame[ey:ey+eh, ex:ex+ew].shape[0],frame[ey:ey+eh, ex:ex+ew].shape[1]))
+            #b = cv2.resize(roi_color[ey:ey+eh, ex:ex+ew], (int(ey), int(ex)))
 
+            print(roi_color[ey:ey+eh, ex:ex+ew].shape, a.shape, frame[ey:ey+eh, ex:ex+ew].shape)
+            c = cv2.add(a,roi_color[ey:ey+eh, ex:ex+ew])
+            roi_color[ey:ey+eh, ex:ex+ew] = c
+            #cv2.imshow('f', c)
+            #print(mask[ey:ey+eh,:], mask[:,ex:ex+ew])
+            #cv2.imshow('d', c)
+            #mask[y:y+h, x:x+w] = res
+            #res = cv2.resize(frame,(2*frame[y:y+h], 2*frame[x:x+w]), interpolation = cv2.INTER_CUBIC)
+
+            #mask[y:y+h, x:x+w] = frame[y:y+h, x:x+w]
+        #frame[y:y+h, x:x+w] = gr
+        # pts1 = np.float32([[56,65],[368,52],[28,387],[389,390]])
+        # pts2 = np.float32([[0,0],[300,0],[0,300],[300,300]])
+        # M = cv2.getPerspectiveTransform(pts1,pts2)
+        width = int(300)
+        height = int(300)
+        #frame.shape[0]/2 - width/2: frame.shape[0]/2 + width/2
+        newimg = cv2.resize(frame[y:y+h, x:x+w],(width,height))
+        #dst = cv2.warpPerspective(frame[y:y+h, x:x+w],M,(300,300))
+        mask[90:390, 170:470] = newimg
+        #cv2.imshow("c",newimg)
+        #cv2.imshow('image', img)
         #recognize
 
         img_item = "my-image.png"
@@ -59,10 +86,10 @@ while(True):
 
         color = (255, 255, 255)
         stroke = 2
-        width = x+w #end cord x
-        height = y+h #end cord y
-        cv2.rectangle(mask, (x,y), (width, height), color, stroke)
 
+
+        cv2.rectangle(frame, (170,90), (470, 390), color, stroke)
+        #cv2.imshow('frame', frame)
 
     # mostra i frame
     #res = cv2.bitwise_and(frame, frame, mask=mask)
