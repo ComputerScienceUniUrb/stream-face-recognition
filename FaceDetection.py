dc4db9f3a904ee527834493f7f0c5f8659343f70
Bilashos <36623574+Bilashos@users.noreply.github.com>
2019-08-30 22:33:42 +0200

    modifiche
    modifica per la portabilità su schermate di grandezza diverse, aggiunta del filtro monocolo

--- FaceDetection.py
+++ FaceDetection.py
@@ -14,7 +14,7 @@ cv2.createTrackbar("UV", "Tracking",255,255,nothing)
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-img = cv2.imread('download2.png')
+img = cv2.imread('monocolo.png')
 cap = cv2.VideoCapture(0)
 
 while(True):
@@ -48,17 +48,27 @@ while(True):
 
 
         eyes = eye_cascade.detectMultiScale(roi_gray)
+        f = 1
         for(ex,ey,ew,eh) in eyes:
             color = (255, 255, 255)
-            cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), color, 2)
+            #cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), color, 2)
             #b = np.delete(roi_color[ey:ey+eh, ex:ex+ew], 3, axis=1)
             #print(b[0],b[1],)
             a = cv2.resize(img, (frame[ey:ey+eh, ex:ex+ew].shape[0],frame[ey:ey+eh, ex:ex+ew].shape[1]))
             #b = cv2.resize(roi_color[ey:ey+eh, ex:ex+ew], (int(ey), int(ex)))
+            laplacian = cv2.Laplacian(a, cv2.CV_8U)
+            sobelx = cv2.Sobel(img,cv2.CV_8U,1,0,ksize=5)
+            sobely = cv2.Sobel(img,cv2.CV_8U,0,1,ksize=5)
 
-            print(roi_color[ey:ey+eh, ex:ex+ew].shape, a.shape, frame[ey:ey+eh, ex:ex+ew].shape)
-            c = cv2.add(a,roi_color[ey:ey+eh, ex:ex+ew])
-            roi_color[ey:ey+eh, ex:ex+ew] = c
+            #cv2.imshow('a',laplacian)
+
+            #print(roi_color[ey:ey+eh, ex:ex+ew].shape, a.shape, frame[ey:ey+eh, ex:ex+ew].shape)
+            #da avere il monocolo solo ad un occhio
+            if (f % 2) != 0:
+                c = cv2.add(roi_color[ey:ey+eh, ex:ex+ew],laplacian)
+                roi_color[ey:ey+eh, ex:ex+ew] = c
+
+            f = f+1
             #cv2.imshow('f', c)
             #print(mask[ey:ey+eh,:], mask[:,ex:ex+ew])
             #cv2.imshow('d', c)
@@ -72,10 +82,15 @@ while(True):
         # M = cv2.getPerspectiveTransform(pts1,pts2)
         width = int(300)
         height = int(300)
+
+        i = int(frame.shape[0]/2 - width/2)
+        j = int(frame.shape[1]/2 - height/2)
+        i_f = int(frame.shape[0]/2 + width/2)
+        j_f = int(frame.shape[1]/2 + height/2)
         #frame.shape[0]/2 - width/2: frame.shape[0]/2 + width/2
         newimg = cv2.resize(frame[y:y+h, x:x+w],(width,height))
         #dst = cv2.warpPerspective(frame[y:y+h, x:x+w],M,(300,300))
-        mask[90:390, 170:470] = newimg
+        mask[i:i_f, j:j_f] = newimg
         #cv2.imshow("c",newimg)
         #cv2.imshow('image', img)
         #recognize
@@ -88,7 +103,7 @@ while(True):
         stroke = 2
 
 
-        cv2.rectangle(frame, (170,90), (470, 390), color, stroke)
+        #cv2.rectangle(frame, (170,90), (470, 390), color, stroke)
         #cv2.imshow('frame', frame)
 
     # mostra i frame
